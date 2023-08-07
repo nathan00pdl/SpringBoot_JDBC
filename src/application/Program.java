3ea0cfea1ab92af13fa35670983e5e7a6eb9241b
Nathan Paiva <nathanpaiva00@gmail.com>
2023-08-07 10:16:45 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import entities.Department;
+import entities.Seller;
 
 public class Program {
 
@@ -8,5 +11,8 @@ public class Program {
 
 		Department obj = new Department(1, "department 1");
 		System.out.println(obj);
+		
+		Seller seller = new Seller(20, "joao", "joao@gmail.com", new Date(), 300.00);
+		System.out.println(seller);
 	}
 }
