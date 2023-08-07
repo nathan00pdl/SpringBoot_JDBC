7ccb2773a4b9a78e14b65f1854acca7ebf7e73f3
Nathan Paiva <nathanpaiva00@gmail.com>
2023-08-07 09:56:19 -0300

    Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import entities.Department;
+
 public class Program {
 
 	public static void main(String[] args) {
 
-		
+		Department obj = new Department(1, "department 1");
+		System.out.println(obj);
 	}
 }
